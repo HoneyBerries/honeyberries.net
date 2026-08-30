ec43b597db3f435c2b415b7172508b3782960d8c
Henry Ng <henry.rainbowfish@gmail.com>
2026-08-30 11:46:04 -0700

    Update `skills.ts` data to replace Minecraft reference with meaningful project count

--- lib/data/skills.ts
+++ lib/data/skills.ts
@@ -13,5 +13,5 @@ export const skills: string[] = [
 export const stats: { value: string; label: string }[] = [
     { value: "7+", label: "Years of programming experience" },
     { value: "A(+)", label: "Grades at Berkeley" },
-    { value: "0", label: "Number of times I have gotten caught cheating in Minecraft"}
+    { value: "5+", label: "Meaningful projects"}
 ]
